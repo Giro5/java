84c1317530f042ca7ba2859656bbdc2d43353ed3
Giro <39918253+Giro5@users.noreply.github.com>
2019-09-19 00:37:03 +0500

    Add files via upload

---
+++ firstproject/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Math.pow(0.5, 0.5));
+        System.out.println(Math.sqrt(0.5));
+        System.out.println(Math.exp(Math.log(0.5) * 0.5));
+
+    }
+}
