bfc1d65db0e2accde2fd81d032205ed380ae2235
Nikita <39918253+Giro5@users.noreply.github.com>
2019-09-27 01:35:42 +0500

    java chroma

---
+++ firstproject/src/com/company/InterfaceClass.java
@@ -0,0 +1,55 @@
+package com.company;
+
+/**
+ * InterfaceClass
+ */
+public class InterfaceClass {
+    
+}
+
+interface InterfaceBase {
+    public String getString();
+}
+
+class ImpClass implements InterfaceBase {
+    @Override
+    public String getString() {
+        return "hello";
+    }
+
+    public int get() {
+        return 1;
+    }
+}
+
+abstract class AbsBase {
+    abstract public int get();
+
+    public int i;
+
+    final public int getI() {
+        return i;
+    }
+}
+
+class ExtClass extends AbsBase {
+    @Override
+    public int get() {
+        return 5;
+    }
+}
+
+class NextClass extends AbsBase {
+    @Override
+    public int get() {
+        return 9;
+    }
+}
+
+class NexyClass extends AbsBase {
+    @Override
+    public int get() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
