bbf41a0ee9afa8402c282b787fcad0e0370d69bd
Nikita <39918253+Giro5@users.noreply.github.com>
2019-10-17 17:02:32 +0500

    cpc done

--- secondproject/src/com/company/Main.java
+++ secondproject/src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        int 1x; - ошибка, переменные не могут начинаться с цифр
+
+//        double t;
+//        int t; - ошибка, переменная уже создана
+
+//        int f,f; - ошибка, переменная уже создана
+
+//        int x,X;
+//        double a;a1; - ошибка, ";" указана вместо ","
+
+//        String kniga1;kniga2; - ошибка, ";" указана вместо ","
+
+//        char zank1,znak2,znak_1; - все правильно, объявляются три переменные
+
+//        int x,a,b;
+//        double y,z,x; - ошибка,  переменная "x" уже создана
+
+//        int x;
+//        double X; - все правильно, объяевление двух переменных
+
+        //int x = -16, t;
+
+        //double t = 5.5, f = 10 * t;
+
+        //int a = 5, b = 9, c = a + b;
+
+        //double x = 7.5, a = x * 2, b = x * 3;
+
+        //int a = 5, b = 8, c = a * 10 + b;
+
+
     }
 }
