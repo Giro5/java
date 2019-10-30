d826a5aed774d1dddf9018cc4cb8fbd80c578644
Nikita <39918253+Giro5@users.noreply.github.com>
2019-10-30 11:05:57 +0500

    sqlite1

--- Appandro/app/src/main/java/com/example/appandro/MainActivity.java
+++ Appandro/app/src/main/java/com/example/appandro/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.appandro;
 
+
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.provider.MediaStore;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.database.sqlite.*;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 0;
+    private ImageView iw;
+    private ConstraintLayout cl;
+    private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
         //создали связь кнопки с объектом
         final Button b = findViewById(R.id.button);
-        final TextView tv = findViewById(R.id.textView);
-        final EditText et = findViewById(R.id.editText2);
+        et = findViewById(R.id.editText2);
         final TextView tv3 = findViewById(R.id.textView3);
+        final Button b36 = findViewById(R.id.button36);
+        iw = findViewById(R.id.imageView);
+        cl = findViewById(R.id.Layout1);
 
         b.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
@@ -36,5 +55,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        b36.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                //intent.setAction(Intent.ACTION_CAMERA_BUTTON);
+                //intent.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_CAMERA));
+                //sendOrderedBroadcast(intent, null);
+            }
+        });
+
+        SQLiteDatabase db = openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER);");
+        //db.execSQL("insert into users values('oleg', 34);");
+        Cursor query = db.rawQuery("select * from users;", null);
+        if (query.moveToFirst()){
+            et.setText(query.getString(0) + " " + query.getInt(1));
+        }
+        //et.setText(db.getPath());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK){
+            Bitmap pic = (Bitmap) data.getExtras().get("data");
+            pic = Bitmap.createScaledBitmap(pic, 1440, 1920, true);
+            iw.setMinimumHeight(1920);
+            iw.setMinimumWidth(1440);
+            et.setText(pic.getWidth() + " " + pic.getHeight());
+//            Bitmap pic = Bitmap.createScaledBitmap(oldpic, 3000, 5400, false);
+//            int picw = pic.getWidth(), pich = pic.getHeight();
+//            et.setText(picw + " " + pich);
+//            for (int i = 0; i < picw; i++){
+//                for (int j = 0; j < pich; j++){
+//                    float[] curcol = new float[3];
+//                    Color.colorToHSV(pic.getPixel(i, j), curcol);
+//                    curcol[1] = 0;
+//                    pic.setPixel(i, j, Color.HSVToColor(curcol));
+//                }
+//            }
+            //Drawable dr = new BitmapDrawable(pic);
+//            cl.setBackground(dr);
+
+            iw.setImageBitmap(pic);
+        }
     }
 }
---
+++ firstproject/src/com/company/SQLiteFor.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * SQLiteFor
+ */
+public class SQLiteFor {
+
+    public static void main(String[] args) {
+        
+    }
+}
