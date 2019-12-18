0c51671b81b3717d0952ee65fb786a319f00431b
Nikita <39918253+Giro5@users.noreply.github.com>
2019-12-18 08:26:09 +0500

    h

--- Appandro/app/src/main/java/com/example/appandro/MainActivity.java
+++ Appandro/app/src/main/java/com/example/appandro/MainActivity.java
@@ -10,17 +10,20 @@ import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import android.database.sqlite.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 0;
@@ -76,15 +79,28 @@ public class MainActivity extends AppCompatActivity {
         //et.setText(db.getPath());
     }
 
+    String mCurrentPhotoPath;
+    private File createImg() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp+"_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,".jpg",storageDir);
+        mCurrentPhotoPath = "file:"+image.getAbsolutePath();
+        return image;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK){
+
             Bitmap pic = (Bitmap) data.getExtras().get("data");
-            pic = Bitmap.createScaledBitmap(pic, 1440, 1920, true);
-            iw.setMinimumHeight(1920);
-            iw.setMinimumWidth(1440);
-            et.setText(pic.getWidth() + " " + pic.getHeight());
+            //et.setText(pic.getWidth() + " " + pic.getHeight());
+//            et.setText();
+            //pic = Bitmap.createScaledBitmap(pic, 3000, 4000, true);
+//            iw.setMinimumHeight(4000);
+//            iw.setMinimumWidth(3000);
+//            et.setText(pic.getWidth() + " " + pic.getHeight());
 //            Bitmap pic = Bitmap.createScaledBitmap(oldpic, 3000, 5400, false);
 //            int picw = pic.getWidth(), pich = pic.getHeight();
 //            et.setText(picw + " " + pich);
